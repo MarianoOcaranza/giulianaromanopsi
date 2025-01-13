3a64c9a80f18ec76ff4b83d6aec332a44efde456
Mariano Ocaranza <marianoocaranza@gmail.com>
2025-01-13 13:51:09 -0300

    Header finished

--- Giuliana_Website/src/components/common/Header.tsx
+++ Giuliana_Website/src/components/common/Header.tsx
@@ -17,12 +17,11 @@ export default function Header() {
   }, []);
  
   const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="mt-2 mb-4 text-slate-100 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
         as="li"
         variant="small"
-        color="blue-gray"
-        className="flex items-center gap-x-2 p-1 font-medium text-base"
+        className="hover:text-black transition duration-250 flex items-center gap-x-2 p-1 font-medium text-base"
       ><a href="/" className="flex items-center">
           Inicio
         </a>
@@ -30,8 +29,7 @@ export default function Header() {
       <Typography
         as="li"
         variant="small"
-        color="blue-gray"
-        className="flex items-center gap-x-2 p-1 font-medium text-base"
+        className="hover:text-black transition duration-250 flex items-center gap-x-2 p-1 font-medium text-base"
       >
         <a href="/About" className="flex items-center">
           Sobre mi
@@ -40,8 +38,7 @@ export default function Header() {
       <Typography
         as="li"
         variant="small"
-        color="blue-gray"
-        className="flex items-center gap-x-2 p-1 font-medium text-base"
+        className="hover:text-black transition duration-250 flex items-center gap-x-2 p-1 font-medium text-base"
       >
         <a href="/Contact" className="flex items-center">
           Contacto
@@ -51,7 +48,7 @@ export default function Header() {
   );
  
   return (
-    <Navbar className="mx-auto bg-violet-600 px-4 py-2 lg:px-8 lg:py-4">
+    <Navbar className="mx-auto bg-violet-900 px-4 py-2 lg:px-8 lg:py-4">
       <div className="container mx-auto flex items-center justify-around text-slate-100">
         <Typography
           as="a"
