d5160ca2617899871e8781688afe1445169eef51
Mariano Ocaranza <marianoocaranza@gmail.com>
2025-01-14 20:28:46 -0300

    Landing page finished TODO: about page

--- Giuliana_Website/src/layouts/Landing.tsx
+++ Giuliana_Website/src/layouts/Landing.tsx
@@ -8,7 +8,7 @@ const Landing: React.FC = ()=> {
             <div className="flex bg-purple-400 flex-col lg:flex-row md:items-center md:min-h-screen">
                 <Presentation />
                 <img src={Image} alt="main_image" 
-                    className="w-full lg:w-1/2 md:w-3/4 object-cover max-w-full m-auto"
+                    className="w-full lg:w-1/2 md:w-3/4 object-cover max-w-full m-auto rounded shadow-2xl shadow-black"
                 />
             </div>
         </>
