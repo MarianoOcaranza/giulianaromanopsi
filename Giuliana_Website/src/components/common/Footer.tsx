b7e3d05464ecb4838daf1b618e38ac672fbfbd24
Mariano Ocaranza <marianoocaranza@gmail.com>
2025-06-24 23:50:32 -0300

    algunos problemas resueltos

--- Giuliana_Website/src/components/common/Footer.tsx
+++ Giuliana_Website/src/components/common/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = ()=> {
     return (
         <>
-            <div className="bg-purple-600 mt-5 text-center w-full text-slate-100">Copyright © 2025 Mariano Ocaranza | Soluciones Web. @v1.5
+            <div className="bg-purple-600 mt-5 text-center w-full bottom-0 text-slate-100">Copyright © 2025 Mariano Ocaranza | Soluciones Web. @v1.5
             </div>
         </>
     )
--- Giuliana_Website/src/layouts/MainLayout.tsx
+++ Giuliana_Website/src/layouts/MainLayout.tsx
@@ -5,9 +5,9 @@ import Footer from '../components/common/Footer';
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
    
     return (
-      <div className="relative min-h-screen flex flex-col justify-center items-center overflow-hidden bg-gradient-to-r from-purple-500 to-purple-300">
+      <div className="min-h-screen flex flex-col items-center justify-between overflow-hidden bg-gradient-to-r from-purple-500 to-purple-300">
         <Header />
-        <main className="container mx-auto px-4 relative z-10">{children}</main>
+          <main className="container mx-auto px-4 relative z-10">{children}</main>
         <Footer />
       </div>
     );
